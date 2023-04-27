e09fcdf6d943eae8938ac6ec4fba81c001c2c73a
Fardhan Mauladi <fardhan16@gmail.com>
2023-04-27 07:13:37 +0700

    About update

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
             </div>
             <div className="w-1/2 flex flex-col items-center self-center">
               <AnimatedText text="Turning Vision Into Reality With Code And Design." className="!text-6xl !text-left" />
-              <p className="my-4 text-base font-medium ">
+              <p className="my-4 text-base font-medium">
                 As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. Explore my latest projects and articles, showcasing my expertise in React.js and web development.
               </p>
               <div className="flex items-center self-start mt-2">
