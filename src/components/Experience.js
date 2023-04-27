b35fa682f0d599c787c19e8f6960bc13504a6b09
Fardhan Mauladi <fardhan16@gmail.com>
2023-04-27 23:09:45 +0700

    update exp +

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -8,15 +8,12 @@ const Details = ({ position, company, companyLink, time, address, work }) => {
     <li ref={ref} className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between">
       <LiIcon reference={ref} />
       <motion.div initial={{ y: 50 }} whileInView={{ y: 0 }} transition={{ duration: 0.5, type: "spring" }}>
-        <h3 className="capitalize font-bold text-2xl">
-          {position}&nbsp;
-          <h4>
-            {" "}
-            <a href={companyLink} target="_blank" className="text-primary capitalize">
-              @{company}
-            </a>
-          </h4>
-        </h3>
+        <h3 className="capitalize font-bold text-2xl">{position}&nbsp;</h3>
+        <h4 className="capitalize font-bold text-lg">
+          <a href={companyLink} target="_blank" className="text-primary capitalize">
+            @{company}
+          </a>
+        </h4>
         <span className="capitalize font-medium text-dark/75 ">
           {time} | {address}
         </span>
