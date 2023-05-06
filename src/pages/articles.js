14cbb6abef94a3467ddc8d242b843de40546fc19
Fardhan Mauladi <fardhan16@gmail.com>
2023-05-06 14:42:02 +0700

    add article

---
+++ src/pages/articles.js
@@ -0,0 +1,134 @@
+import AnimatedText from "@/components/AnimatedText";
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useRef } from "react";
+import article1 from "../../public/images/articles/article1.jpg";
+import article2 from "../../public/images/articles/article2.jpg";
+import article3 from "../../public/images/articles/article3.png";
+import article4 from "../../public/images/articles/article4.png";
+import article5 from "../../public/images/articles/article5.png";
+import article6 from "../../public/images/articles/article6.jpg";
+import article7 from "../../public/images/articles/article7.png";
+import article8 from "../../public/images/articles/article8.png";
+
+import { motion, useMotionValue } from "framer-motion";
+
+const FramerImage = motion(Image);
+
+const MovingImg = ({ title, img, link }) => {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const imgRef = useRef(null);
+
+  function handleMouse(event) {
+    imgRef.current.style.display = "inline-block";
+    x.set(event.pageX);
+    y.set(-10);
+  }
+  function handleMouseLeave() {
+    imgRef.current.style.display = "none";
+    x.set(0);
+    y.set(0);
+  }
+
+  return (
+    <Link href={link} target="_blank" onMouseMove={handleMouse} onMouseLeave={handleMouseLeave}>
+      <h2 className="capitalize text-xl font-semibold hover:underline">{title}</h2>
+      <FramerImage style={{ x: x, y: y }} initial={{ opacity: 0 }} whileInView={{ opacity: 1, transition: { duration: 0.3 } }} ref={imgRef} src={img} alt={title} className="z-10 w-96 h-auto hidden absolute rounded-lg" />
+    </Link>
+  );
+};
+
+const Article = ({ img, title, date, link }) => {
+  return (
+    <motion.li
+      initial={{ y: 200 }}
+      whileInView={{ y: 0, transition: { duration: 0.5, ease: "easeInOut" } }}
+      viewport={{ once:true }}
+      className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4"
+    >
+      <MovingImg title={title} img={img} link={link} />
+      <span className="text-primary font-semibold pl-4">{date}</span>
+    </motion.li>
+  );
+};
+
+const FeaturedArticle = ({ img, title, time, summary, link }) => {
+  return (
+    <li className="col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl relative">
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl" />
+      <Link href={link} target="_blank" className="w-full inline-block cursor-pointer overflow-hidden rounded-lg">
+        <FramerImage src={img} alt={title} className="w-full h-auto" whileHover={{ scale: 1.05 }} transition={{ duration: 0.2 }} />
+      </Link>
+      <Link href={link} target="_blank">
+        <h2 className="capitalize text-2xl font-bold my-2 mt-4 hover:underline">{title}</h2>
+      </Link>
+      <p className="text-sm mb-2">{summary}</p>
+      <span className="text-primary font-semibold">{time}</span>
+    </li>
+  );
+};
+
+const articles = () => {
+  return (
+    <>
+      <Head>
+        <title>Things UIX | Articles</title>
+        <meta name="description" content="any description" />
+      </Head>
+      <main className="w-full mb-16 flex flex-col items-center justify-center">
+        <Layout className="pt-16">
+          <AnimatedText text="Words Can Change The World!" className="mb-16" />
+          <ul className="grid grid-cols-2 gap-16">
+            <FeaturedArticle
+              img={article1}
+              title="Build A Custom Pagination Component In Reactjs From Scratch"
+              summary="Learn how to build a custom pagination component in ReactJS from scratch. 
+            Follow this step-by-step guide to integrate Pagination component in your ReactJS project."
+              time="9 min read"
+              link="/"
+            />
+            <FeaturedArticle
+              img={article2}
+              title="Creating Stunning Loading Screens In React: Build 3 Types Of Loading Screens"
+              summary="Learn how to create stunning loading screens in React with 3 different methods. 
+              Discover how to use React-Loading, React-Lottie & build a custom loading screen. 
+              Improve the user experience."
+              time="15 min read"
+              link="/"
+            />
+            <FeaturedArticle
+              img={article3}
+              title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+              summary="Learn how to create stunning loading screens in React with 3 different methods. 
+              Discover how to use React-Loading, React-Lottie & build a custom loading screen. 
+              Improve the user experience."
+              time="10 min read"
+              link="/"
+            />
+            <FeaturedArticle
+              img={article4}
+              title="Redux Simplified: A Beginner's Guide For Web Developers"
+              summary="Learn how to create stunning loading screens in React with 3 different methods. 
+              Discover how to use React-Loading, React-Lottie & build a custom loading screen. 
+              Improve the user experience."
+              time="20 min read"
+              link="/"
+            />
+          </ul>
+          <h2 className="font-bold text-4xl w-full text-center my-16 mt-32">All Articles</h2>
+          <ul>
+            <Article title="What Is Higher Order Component (Hoc) In React?" date="Jan 22, 2023" link="/" img={article6} />
+            <Article title="Silky Smooth Scrolling In Reactjs: A Step-By-Step Guide For React Developers" date="Feb 17, 2023" link="/" img={article5} />
+            <Article title="Build A Fabulous Todo List App With React, Redux And Framer-Motion" date="March 27, 2023" link="/" img={article7} />
+            <Article title="Creating An Efficient Modal Component In React Using Hooks And Portals" date="Dec 15, 2022" link="/" img={article8} />
+          </ul>
+        </Layout>
+      </main>
+    </>
+  );
+};
+
+export default articles;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,22 +6,27 @@ import Link from "next/link";
 import Image from "next/image";
 import { GithubIcon } from "@/components/Icons";
 import selaras from "../../public/images/projects/selaras.png";
+import { motion } from "framer-motion";
+
+const FramerImage = motion(Image);
 
 const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   return (
-    <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl">
-      <Link href={link} target="_blank" className="w-1/2 cursor-pointer overflow-hidden rounded-3xl p-12">
-        <Image src={img} alt={title} className="w-full h-auto" />
+    <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 relative rounded-br-2xl">
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl" />
+      <Link href={link} target="_blank" className="w-1/2 cursor-pointer overflow-hidden rounded-lg">
+        <FramerImage src={img} alt={title} className="w-full h-auto" whileHover={{ scale: 1.05 }} transition={{ duration: 0.2 }} />
       </Link>
       <div className="w-1/2 flex flex-col items-start justify-between pl-6">
         <span className="text-primary font-medium text-xl">{type}</span>
         <Link href={link} target="_blank" className="hover:underline underline-offset-2">
-          <h2 className="my-2 w-full text-left text-4xl font-bold">{title}</h2>
+          <h2 className="my-2 w-full text-left text-3xl font-bold">{title}</h2>
         </Link>
-        <p className="my-2 font-medium text-dark">{summary}</p>
+        <p className="my-2 font-medium text-dark mr-2">{summary}</p>
         <div className="mt-2 flex items-center">
           <Link href={github} target="_blank" className="w-10">
-            <GithubIcon />
+            {" "}
+            <GithubIcon />{" "}
           </Link>
           <Link href={link} target="_blank" className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold">
             Visit Project
@@ -34,22 +39,24 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
 
 const Project = ({ title, type, img, link, github }) => {
   return (
-    <article className="w-full flex items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative">
+    <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative">
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl" />
       <Link href={link} target="_blank" className="w-full cursor-pointer overflow-hidden rounded-lg">
-        <Image src={img} alt={title} className="w-full h-auto" />
+        <FramerImage src={img} alt={title} className="w-full h-auto" whileHover={{ scale: 1.05 }} transition={{ duration: 0.2 }} />
       </Link>
-      <div className="w-full flex flex-col items-start justify-between pl-6">
+      <div className="w-full flex flex-col items-start justify-between mt-4">
         <span className="text-primary font-medium text-xl">{type}</span>
         <Link href={link} target="_blank" className="hover:underline underline-offset-2">
-          <h2 className="my-2 w-full text-left text-4xl font-bold">{title}</h2>
+          <h2 className="my-2 w-full text-left text-2xl font-bold">{title}</h2>
         </Link>
 
-        <div className="mt-2 flex items-center">
-          <Link href={github} target="_blank" className="w-10">
-            <GithubIcon />
-          </Link>
-          <Link href={link} target="_blank" className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold">
+        <div className="w-full mt-2 flex items-center justify-between">
+          <Link href={link} target="_blank" className="text-lg font-semibold underline">
             Visit
+          </Link>
+          <Link href={github} target="_blank" className="w-8">
+            {" "}
+            <GithubIcon />{" "}
           </Link>
         </div>
       </div>
@@ -68,7 +75,7 @@ const projects = () => {
         <Layout className="pt-16">
           <AnimatedText text="Think Positive Do Creative!" className="mb-16" />
 
-          <div className="grid grid-cols-12 gap-24">
+          <div className="grid grid-cols-12 gap-24 gap-y-32">
             <div className="col-span-12">
               <FeaturedProject
                 title="Wedding Organizer Website"
@@ -89,10 +96,46 @@ const projects = () => {
                 github="/"
               />
             </div>
-            <div className="col-span-6">Project-2</div>
-            <div className="col-span-12">Feature Project</div>
-            <div className="col-span-6">Project-3</div>
-            <div className="col-span-6">Project-4</div>
+            <div className="col-span-6">
+              <Project
+                title="Wedding Organizer Website"
+                img={selaras}
+                summary="Website landing page about Wedding Organizer for Selaras Wedding Organizer. Create with HTML, CSS with Bootstrap. you can see portfolio about wedding organizer"
+                link="https://www.selaras-organizer.com/"
+                type="Featured Project"
+                github="/"
+              />
+            </div>
+            <div className="col-span-12">
+              <FeaturedProject
+                title="Wedding Organizer Website"
+                img={selaras}
+                summary="Website landing page about Wedding Organizer for Selaras Wedding Organizer. Create with HTML, CSS with Bootstrap. you can see portfolio about wedding organizer"
+                link="https://www.selaras-organizer.com/"
+                type="Featured Project"
+                github="/"
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                title="Wedding Organizer Website"
+                img={selaras}
+                summary="Website landing page about Wedding Organizer for Selaras Wedding Organizer. Create with HTML, CSS with Bootstrap. you can see portfolio about wedding organizer"
+                link="https://www.selaras-organizer.com/"
+                type="Featured Project"
+                github="/"
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                title="Wedding Organizer Website"
+                img={selaras}
+                summary="Website landing page about Wedding Organizer for Selaras Wedding Organizer. Create with HTML, CSS with Bootstrap. you can see portfolio about wedding organizer"
+                link="https://www.selaras-organizer.com/"
+                type="Featured Project"
+                github="/"
+              />
+            </div>
           </div>
         </Layout>
       </main>
